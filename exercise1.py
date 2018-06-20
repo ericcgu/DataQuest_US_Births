10050c5475d7573b5c179073dcfb9b72d1cd9d53
Eric Gu <eric.changning.gu@gmail.com>
2018-06-20 14:15:45 -0400

    exercise 1 complete

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+#Read the CSV file "US_births_1994-2003_CDC_NCHS.csv" into a string.
+#Split the string on the newline character ("\n").
+#Display the first 10 values in the resulting list.
+
+f = open('US_births_1994-2003_CDC_NCHS.csv', 'r')
+data = f.read()
+data_list = data.split('\n')
+
+print(data_list[:10])
