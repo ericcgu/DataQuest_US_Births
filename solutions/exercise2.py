59d50889db2143f35bb1da64ebbde54065f4f613
Eric Gu <eric.changning.gu@gmail.com>
2018-06-20 15:14:52 -0400

    added instructions to exercise 2

--- solutions/exercise2.py
+++ solutions/exercise2.py
@@ -1,3 +1,18 @@
+'''
+Create a function named read_csv() that:
+Takes a single, required argument, a string representing the file name of the CSV file.
+Reads the file into a string, splits the string on the newline character ("\n"), and removes the header row. Assign this list to string_list and create an empty list named final_list.
+Uses a for loop to:
+Iterate over string_list,
+Create an empty list named int_fields,
+Splits each row on the comma delimiter (,) and assigns the resulting list to string_fields,
+Converts each value in string_fields to an integer and appends to int_fields,
+Appends int_fields to final_list.
+Returns final_list.
+Use the read_csv() function to read in the file "US_births_1994-2003_CDC_NCHS.csv" and assign the result to cdc_list.
+Display the first 10 rows of cdc_list to confirm it's a list of lists, containing only integer values, and no header row.
+'''
+
 def readcsv(filename):
     with open(filename, 'r', encoding='utf-8') as infile:
         int_fields = []
